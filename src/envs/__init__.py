3f014d5f93c4cb9d9155a660b5e0cbe47ecd3cc2
tiecun <852488062@qq.com>
2022-11-24 20:22:06 +0800

    change the sc2 location

--- src/envs/__init__.py
+++ src/envs/__init__.py
@@ -10,5 +10,6 @@ REGISTRY = {}
 REGISTRY["sc2"] = partial(env_fn, env=StarCraft2Env)
 
 if sys.platform == "linux":
-    os.environ.setdefault("SC2PATH",
-                          os.path.join(os.getcwd(), "3rdparty", "StarCraftII"))
+    # os.environ.setdefault("SC2PATH",
+    #                       os.path.join(os.getcwd(), "3rdparty", "StarCraftII"))
+    os.environ.setdefault("~/StarCraftII")
